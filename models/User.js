b63d8c5eadbf502d9a41ce90db5e19c12a5da833
alishaevn <alieva.inc@gmail.com>
2018-04-16 08:48:17 -0700

    created user schema

---
+++ models/User.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  fullname: {
+    type: String
+  },
+  userName: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
