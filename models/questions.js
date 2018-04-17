e855390212473eaa295b14f1e563711a7ddaa9da
alishaevn <alieva.inc@gmail.com>
2018-04-17 09:15:37 -0700

    moved the question/answer/M into a value prop for each node

--- models/questions.js
+++ models/questions.js
@@ -3,85 +3,125 @@
 let questions = [
 
   {
-    question: 'bread',
-    answer: 'le pain',
-    M: 1,
+    value: {
+      question: 'bread',
+      answer: 'le pain',
+      M: 1
+    },
     next: {
-      question: 'coffee',
-      answer: 'le cafe',
-      M: 1,
+      value:{
+        question: 'coffee',
+        answer: 'le cafe',
+        M: 1
+      },
       next: {
-        question: 'banana',
-        answer: 'banana',
-        M: 1,
+        value:{
+          question: 'banana',
+          answer: 'banana',
+          M: 1
+        },
         next: {
-          question: 'chicken',
-          answer: 'le poulet',
-          M: 1,
+          value:{
+            question: 'chicken',
+            answer: 'le poulet',
+            M: 1
+          },
           next: {
-            question: 'vegetables',
-            answer: 'des legumes',
-            M: 1,
+            value:{
+              question: 'vegetables',
+              answer: 'des legumes',
+              M: 1
+            },
             next: {
-              question: 'fruit',
-              answer: 'le fruit',
-              M: 1,
+              value:{
+                question: 'fruit',
+                answer: 'le fruit',
+                M: 1
+              },
               next: {
-                question: 'milk',
-                answer: 'le lait',
-                M: 1,
+                value:{
+                  question: 'milk',
+                  answer: 'le lait',
+                  M: 1
+                },
                 next: {
-                  question: 'butter',
-                  answer: 'le beurre',
-                  M: 1,
+                  value:{
+                    question: 'butter',
+                    answer: 'le beurre',
+                    M: 1
+                  },
                   next: {
-                    question: 'cake',
-                    answer: 'le gateau',
-                    M: 1,
+                    value:{
+                      question: 'cake',
+                      answer: 'le gateau',
+                      M: 1
+                    },
                     next: {
-                      question: 'pie',
-                      answer: 'la tarte',
-                      M: 1,
+                      value:{
+                        question: 'pie',
+                        answer: 'la tarte',
+                        M: 1
+                      },
                       next: {
-                        question: 'onion',
-                        answer: 'le oignon',
-                        M: 1,
+                        value:{
+                          question: 'onion',
+                          answer: 'le oignon',
+                          M: 1
+                        },
                         next: {
-                          question: 'eggs',
-                          answer: 'des oeufs',
-                          M: 1,
+                          value:{
+                            question: 'eggs',
+                            answer: 'des oeufs',
+                            M: 1
+                          },
                           next: {
-                            question: 'cheese',
-                            answer: 'le fromage',
-                            M: 1,
+                            value:{
+                              question: 'cheese',
+                              answer: 'le fromage',
+                              M: 1
+                            },
                             next: {
-                              question: 'french fries',
-                              answer: 'les frites',
-                              M: 1,
+                              value:{
+                                question: 'french fries',
+                                answer: 'les frites',
+                                M: 1
+                              },
                               next: {
-                                question: 'chocolate',
-                                answer: 'le chocolat',
-                                M: 1,
+                                value:{
+                                  question: 'chocolate',
+                                  answer: 'le chocolat',
+                                  M: 1
+                                },
                                 next: {
-                                  question: 'sugar',
-                                  answer: 'le sucre',
-                                  M: 1,
+                                  value:{
+                                    question: 'sugar',
+                                    answer: 'le sucre',
+                                    M: 1
+                                  },
                                   next: {
-                                    question: 'cream',
-                                    answer: 'la creme',
-                                    M: 1,
+                                    value:{
+                                      question: 'cream',
+                                      answer: 'la creme',
+                                      M: 1
+                                    },
                                     next: {
-                                      question: 'carrot',
-                                      answer: 'le carotte',
-                                      M: 1,
+                                      value:{
+                                        question: 'carrot',
+                                        answer: 'le carotte',
+                                        M: 1
+                                      },
                                       next: {
-                                        question: 'apple',
-                                        answer: 'la pomme',
-                                        M: 1,
+                                        value:{
+                                          question: 'apple',
+                                          answer: 'la pomme',
+                                          M: 1
+                                        },
                                         next: {
-                                          question: 'orange',
-                                          answer: 'las orange',
-                                          M: 1,
+                                          value:{
+                                            question: 'orange',
+                                            answer: 'las orange',
+                                            M: 1
+                                          },
                                           next: null
                                         }}}}}}}}}}}}}}}}}}}}
 ];
