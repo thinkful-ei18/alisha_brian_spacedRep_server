bcc474ef5517b58b92abb69ea16e0698fa1253d6
alishaevn <alieva.inc@gmail.com>
2018-04-17 11:31:37 -0700

    added questions route

--- routes/local-user-route.js
+++ routes/local-user-route.js
@@ -10,7 +10,7 @@ const User = require('../models/User');
 const options = { session: false, failWithError: true };
 const jwtAuth = passport.authenticate('jwt', options);
 
-/* =========== GET =========== */
+/* =========== GET USER =========== */
 router.get('/', jwtAuth, (req, res, next) => {
 
   const userId = req.user.id;
@@ -18,6 +18,20 @@ router.get('/', jwtAuth, (req, res, next) => {
   User.findById(userId)
     .then( user => {
       res.json(user);
+    })
+    .catch(err => next);
+});
+
+
+/* =========== GET QUESTION HEAD FOR USER =========== */
+router.get('/question', jwtAuth, (req, res, next) => {
+
+  const userId = req.user.id;
+
+  User.findById(userId)
+    .then( user => {
+      console.log('USER:', user);
+      res.json(user.questions[0].value);
     })
     .catch(err => next);
 });
