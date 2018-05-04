7b61a0ca6dc43a3374c42ddbf4e1fba3f3cee7ff
alishaevn <alieva.inc@gmail.com>
2018-05-03 17:23:32 -0700

    removed "head" object

---
+++ seedData/questions.js
@@ -0,0 +1,74 @@
+'use strict';
+
+let questions = [
+  {
+    question: 'le pain',
+    answer: 'bread'
+  },
+  {
+    question: 'le cafe',
+    answer: 'coffee'
+  },
+  {
+    question: 'le poulet',
+    answer: 'chicken'
+  },
+  {
+    question: 'des legumes',
+    answer: 'vegetable'
+  },
+  {
+    question: 'le lait',
+    answer: 'milk'
+  },
+  {
+    question: 'le beurre',
+    answer: 'butter'
+  },
+  {
+    question: 'le gateau',
+    answer: 'cake'
+  },
+  {
+    question: 'la tarte',
+    answer: 'pie'
+  },
+  {
+    question: 'le oignon',
+    answer: 'onion'
+  },
+  {
+    question: 'des oeufs',
+    answer: 'eggs'
+  },
+  {
+    question: 'le fromage',
+    answer: 'cheese'
+  },
+  {
+    question: 'les frites',
+    answer: 'french fries'
+  },
+  {
+    question: 'le chocolat',
+    answer: 'chocolate'
+  },
+  {
+    question: 'le sucre',
+    answer: 'sugar'
+  },
+  {
+    question: 'la creme',
+    answer: 'cream'
+  },
+  {
+    question: 'le carotte',
+    answer: 'carrot'
+  },
+  {
+    question: 'la pomme',
+    answer: 'apple'
+  }
+];
+
+module.exports = questions;
