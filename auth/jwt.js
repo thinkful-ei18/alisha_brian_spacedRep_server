74c2f141fed7ba93e1adde4b34fed3c41dad2bfd
alishaevn <alieva.inc@gmail.com>
2018-05-04 19:49:33 -0700

    cleaned up

--- auth/jwt.js
+++ auth/jwt.js
@@ -1,18 +1,20 @@
+'use strict';
+
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const config = require('../config');
 const passport = require('passport');
 
 //Setting up JWT login strategy
-const { JWT_EXPIRY, JWT_SECRET } = require('../config');
+const { JWT_SECRET } = require('../config');
 
 const options = {
-	secretOrKey: JWT_SECRET,
-	jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
-	algorithms: ['HS256']
+  secretOrKey: JWT_SECRET,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+  algorithms: ['HS256']
 };
 
 const jwtStrategy = new JwtStrategy(options, (payload, done) => {
-	done(null, payload.user);
+  done(null, payload.user);
 });
 
 //JWT End
